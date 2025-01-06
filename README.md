6bdb44e8a2480cb97955eb4ab3956c898f2ffa94
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2025-01-06 17:06:10 +0100

    fa

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # do-fastapi-auth
 An application based on FastAPI with Authentication for Digital Ocean App platform
+
+[![Deploy to DigitalOcean](https://www.deploytodo.com/do-btn-blue.svg)](https://cloud.digitalocean.com/apps/new?repo=https://github.com/skipperkongen/do-fastapi-auth/tree/main)
+
+## Run locally
+
+```shell
+fastapi dev main.py
+```
+
+## Deploy
+
+Using CLI:
+
+```shell
+doctl apps create --spec .do/app.yaml
+```
--- main.py
+++ main.py
@@ -6,24 +6,3 @@ app = FastAPI()
 @app.get('/')
 async def root():
     return {'message': 'hello world'}
-
-
-@app.get('/title/std')
-async def make_title():
-    return {'title_case': 'this is the standard message'.title()}
-
-
-@app.get('/title/{message}')
-async def make_title2(message: str):
-    return {'title_case': message.title()}
-
-fake_items_db = [
-    {"item_name": "Foo"},
-    {"item_name": "Bar"},
-    {"item_name": "Baz"}
-]
-
-
-@app.get('/items')
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
